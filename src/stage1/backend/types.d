627d941564bcaef40e95675d2bbb9c6abfbce6b4
FeepingCreature <default_357-line@yahoo.de>
2020-05-28 21:32:19 +0200

    add backend type toString

--- src/stage1/backend/types.d
+++ src/stage1/backend/types.d
@@ -1,22 +1,33 @@
 module backend.types;
 
 import boilerplate;
+import std.format;
 
 interface BackendType
 {
 }
 
-class BackendVoidType : BackendType { }
+class BackendVoidType : BackendType {
+    override string toString() const { return "void"; }
+}
 
-class BackendCharType : BackendType { }
+class BackendCharType : BackendType {
+    override string toString() const { return "char"; }
+}
 
-class BackendIntType : BackendType { }
+class BackendIntType : BackendType {
+    override string toString() const { return "int"; }
+}
 
-class BackendLongType : BackendType { }
+class BackendLongType : BackendType {
+    override string toString() const { return "long"; }
+}
 
 class BackendPointerType : BackendType
 {
     BackendType target;
+
+    override string toString() const { return format!"%s*"(target); }
 
     mixin(GenerateThis);
 }
@@ -24,6 +35,8 @@ class BackendPointerType : BackendType
 class BackendStructType : BackendType
 {
     BackendType[] members;
+
+    override string toString() const { return format!"{ %(%s, %) }"(members); }
 
     mixin(GenerateThis);
 }
@@ -33,6 +46,8 @@ class BackendFunctionPointerType : BackendType
     BackendType returnType;
 
     BackendType[] argumentTypes;
+
+    override string toString() const { return format!"%s(%(%s, %))"(returnType, argumentTypes); }
 
     mixin(GenerateThis);
 }

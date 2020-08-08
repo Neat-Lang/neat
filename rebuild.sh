44c117934d233d25cbdec645ff7ce168d0a04ec3
FeepingCreature <default_357-line@yahoo.de>
2020-08-08 21:30:28 +0000

    fix: rebuild to convergence

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 set -euxo pipefail
-build/stage2 -Isrc/stage2 main.cx -o build/stage2_test1
-build/stage2_test1 -Isrc/stage2 main.cx -o build/stage2_test2
-build/stage2_test2 -Isrc/stage2 main.cx -o build/stage2
+I=1
+build/stage2 -Isrc/stage2 main.cx -o build/stage2_test$I
+SUM="$(objdump -S build/stage2_test$I |grep -v file\ format |md5sum)"
+SUMNEXT=""
+while true
+do
+    K=$((I+1))
+    build/stage2_test$I -Isrc/stage2 main.cx -o build/stage2_test$K
+    SUMNEXT="$(objdump -S build/stage2_test$K |grep -v file\ format |md5sum)"
+    if [ "$SUM" == "$SUMNEXT" ]; then break; fi
+    SUM="$SUMNEXT"
+    I=$K
+done
+mv build/stage2_test$I build/stage2

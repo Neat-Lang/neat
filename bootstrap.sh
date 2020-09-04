f95a6c4122c8974d028c824bf8c0c2a5811c0092
FeepingCreature <default_357-line@yahoo.de>
2020-09-04 19:42:36 +0000

    - Fix bootstrap - Remove include paths

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,12 +31,18 @@ function dbootstrap {
 }
 function rebuild {
     mkdir build
-    cp ../../build/cx build/stage2
+    cp ../../build/cx build/$1
     ./rebuild.sh
 }
 # before structs in the compiler
 at_revision 'f65eb856f00b3016025b105c4475b1b9c623bdf3' 'dbootstrap' 'build/stage2'
 # move runtime.c to src/
-at_revision 'fd26349126358e19671f5e11f4a67e615e454b5f' 'rebuild' 'build/stage2'
+at_revision 'fd26349126358e19671f5e11f4a67e615e454b5f' 'rebuild stage2' 'build/stage2'
+# idk, something about floats
+at_revision 'edde8f615d72034d7fe645226c43156238895c09' 'rebuild cx' 'build/cx'
+# float literals
+at_revision 'e88bcc52f621c7e47f3a667554d7401c5d72be2e' 'rebuild cx' 'build/cx'
+# package includes
+at_revision 'f6078a3470f7826b566b7eed70acd91f3c647960' 'rebuild cx' 'build/cx'
 ./rebuild.sh
 echo "=== build/cx from master ==="
--- rebuild.sh
+++ rebuild.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -euxo pipefail
 I=1
-build/cx -O -Isrc src/main.cx -o build/cx_test$I
+build/cx -O -Psrc:src src/main.cx -o build/cx_test$I
 SUM="$(objdump -S build/cx_test$I |grep -v file\ format |md5sum)"
 SUMNEXT=""
 while true
 do
     K=$((I+1))
-    build/cx_test$I -O -Isrc -Psrc:src src/main.cx -o build/cx_test$K
+    build/cx_test$I -O -Psrc:src src/main.cx -o build/cx_test$K
     SUMNEXT="$(objdump -S build/cx_test$K |grep -v file\ format |md5sum)"
     if [ "$SUM" == "$SUMNEXT" ]; then break; fi
     SUM="$SUMNEXT"

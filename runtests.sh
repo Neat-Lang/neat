72976b1bd3956da9615dd8e71b6eb6e11d361a8d
FeepingCreature <default_357-line@yahoo.de>
2020-09-06 12:49:29 +0000

    add "supposed-to-fail" (but not crash) tests add, fix first afl testcase

--- runtests.sh
+++ runtests.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 set -euo pipefail
-mkdir -p build/test/runnable
-num_runnable=$(ls test/runnable |wc -l)
+CX=build/cx
+CXFLAGS="-Pcompiler:build/src"
+CXFLAGS="${CXFLAGS} -Prunnable:test/runnable:compiler"
+CXFLAGS="${CXFLAGS} -Pfail_compilation:test/fail_compilation:compiler"
+
+num_total=$(ls test/runnable test/fail_compilation |wc -l)
 build_failed=0
 run_failed=0
+build_crashed=0
+falsely_succeeded=0
+
+# runnable
+mkdir -p build/test/runnable
 while read file
 do
     echo test/runnable/"$file"...
     executable=build/test/runnable/"$file"
-    if ! build/cx -Pcompiler:build/src -Ptest:test/runnable:compiler test/runnable/"$file" -o "$executable" \
-        2>&1 |cat>build/out.txt
+    if ! $CX $CXFLAGS test/runnable/"$file" -o "$executable" 2>&1 |cat>build/out.txt
     then
         build_failed=$((build_failed+1))
         cat build/out.txt
@@ -18,6 +26,35 @@ do
         run_failed=$((run_failed+1))
     fi
 done < <(ls test/runnable)
-num_success=$((num_runnable - build_failed - run_failed))
-echo "Result: ${num_success} successful, ${build_failed} failed to build, ${run_failed} failed to run."
-[ $num_success -eq $num_runnable ]
+
+# fail_compilation
+# tests should fail with an exit code, not a segfault
+mkdir -p build/test/fail_compilation
+while read file
+do
+    echo test/fail_compilation/"$file"...
+    executable=build/test/fail_compilation/"$file"
+    set +e
+    $CX $CXFLAGS test/fail_compilation/"$file" -o "$executable" 2>&1 |cat>build/out.txt
+    EXIT=$?
+    set -e
+    if [ $EXIT -eq 0 ]; then
+        falsely_succeeded=$((falsely_succeeded+1))
+        cat build/out.txt
+        echo "Error expected but not found!"
+    elif [ $EXIT -gt 128 ]; then
+        # signal
+        build_crashed=$((build_crashed+1))
+        cat build/out.txt
+    fi
+done < <(ls test/fail_compilation)
+
+num_success=$((num_total - build_failed - run_failed - falsely_succeeded - build_crashed))
+
+echo "Result:"
+echo "  ${num_success}/${num_total} successful"
+echo "  ${build_failed} failed to build"
+echo "  ${run_failed} failed to run"
+echo "  ${falsely_succeeded} built when they should have failed"
+echo "  ${build_crashed} crashed the compiler instead of erroring"
+[ $num_success -eq $num_total ]

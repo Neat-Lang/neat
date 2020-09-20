66ccc5fb41bb19877697319fc3467a3397f3df2c
FeepingCreature <default_357-line@yahoo.de>
2020-09-20 20:37:09 +0200

    add case{} statement

--- rebuild.sh
+++ rebuild.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 set -euxo pipefail
+FLAGS="-O"
+# FLAGS=""
 if [ ! -d build/src ]
 then
     cp -R src build/src
 fi
 I=1
-build/cx -O -Pcompiler:build/src -Pnext:src src/main.cx -o build/cx_test$I
+build/cx $FLAGS -Pcompiler:build/src -Pnext:src src/main.cx -o build/cx_test$I
 SUM="$(objdump -S build/cx_test$I |grep -v file\ format |md5sum)"
 SUMNEXT=""
 while true
 do
     K=$((I+1))
-    build/cx_test$I -O -Pcompiler:src src/main.cx -o build/cx_test$K
+    build/cx_test$I $FLAGS -Pcompiler:src src/main.cx -o build/cx_test$K
     SUMNEXT="$(objdump -S build/cx_test$K |grep -v file\ format |md5sum)"
     if [ "$SUM" == "$SUMNEXT" ]; then break; fi
     SUM="$SUMNEXT"

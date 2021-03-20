9e6ed73f5005ceb4e7fb776ca9912f0ebbb514aa
FeepingCreature <default_357-line@yahoo.de>
2021-03-20 10:04:20 +0100

    Split function and struct classes out more into struct_ and function_ modules.

--- bootstrap.sh
+++ bootstrap.sh
@@ -271,6 +271,8 @@ at_revision '30aaa695621a169ea8e345bc55a7e55e08668b6c' 'rebuild cx' 'build/cx'
 at_revision 'a588979560782f49f784f02c0f4f4cfed43b32db' 'rebuild cx' 'build/cx'
 # support type conversion on array ~ element
 at_revision '5c9e121ae34b0ebe03b0567a7e4273cbd1be75b3' 'rebuild cx' 'build/cx'
+# accessDecl(CompilerBase) part 1
+at_revision 'd2ca576122c04360f1dc2acea39c93996d15795b' 'rebuild cx' 'build/cx'
 
 # unpack the last tagfile
 unpack_tagfile

688f36565419dc2a68389cdb4aa198d2e9d015a0
FeepingCreature <default_357-line@yahoo.de>
2021-11-01 21:59:43 +0100

    Add double math, step 2: make tests pass

--- bootstrap.sh
+++ bootstrap.sh
@@ -521,6 +521,8 @@ at_revision '4c32f2705ae2017a79b0cc7fbf20a8680411ca62' 'rebuild neat' 'build/nea
 at_revision '367cd56b938cfaf6da30962bf8fe5d11eaa18a82' 'rebuild neat' 'build/neat'
 # public imports
 at_revision '54a48af351bfea2310263668e846c720a86f105a' 'rebuild neat' 'build/neat'
+# double math
+at_revision 'ba68db1edeee04d5fd9ee6c104198206f226bcf6' 'rebuild neat' 'build/neat'
 
 # unpack the last tagfile
 unpack_tagfile

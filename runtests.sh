57b2ec49345c8496630c0d34f562baafa3592c11
FeepingCreature <feepingcreature@gmail.com>
2022-08-19 14:48:44 +0200

    Support access to with namespace from nested functions.

--- runtests.sh
+++ runtests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 NEAT=${NEAT:-build/neat}
-NEATFLAGS="-Pcompiler:build/src -Pimports:test/imports"
+NEATFLAGS="-Pimports:test/imports"
 NEATFLAGS="${NEATFLAGS} -Prunnable:test/runnable:compiler,imports"
 NEATFLAGS="${NEATFLAGS} -Pfail_compilation:test/fail_compilation:compiler,imports"
 

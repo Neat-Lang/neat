b15feb9a8c3f8b2f5b369ea86d8430f0751dbc55
Kelly Wilson <wilsonk@cpsc.ucalgary.ca>
2024-01-06 10:48:34 -0700

    Minor fixes after mutability restrictions in early Jan

--- unittest.sh
+++ unittest.sh
@@ -8,7 +8,23 @@ mkdir -p build
     find src/ -name \*.nt |sed -e 's,/,.,g' -e 's/^src.\(.*\).nt$/import \1;/'
 ) > build/unittest.nt
 
-LLVM_CONFIG=${LLVM_CONFIG:-"/usr/lib/llvm/15/bin/llvm-config"}
+if [ -v LLVM_CONFIG ]
+then
+    :
+elif [ -f "/usr/lib/llvm-15/bin/llvm-config" ]
+then
+    LLVM_CONFIG="/usr/lib/llvm-15/bin/llvm-config"
+elif [ -f "/usr/lib/llvm/15/bin/llvm-config" ]
+then
+    LLVM_CONFIG="/usr/lib/llvm/15/bin/llvm-config"
+elif [ -f "/usr/lib/llvm15/bin/llvm-config" ]
+then
+    LLVM_CONFIG="/usr/lib/llvm15/bin/llvm-config"
+else
+    echo "Cannot find llvm-15 llvm-config!"
+    exit 1
+fi
+
 FLAGS="${FLAGS} -I$($LLVM_CONFIG --includedir) -L-L$($LLVM_CONFIG --libdir)"
 
 neat -unittest -no-main $PACKAGES $FLAGS build/unittest.nt -o build/unittest

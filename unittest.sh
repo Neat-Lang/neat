a0f4c451fa2f6644a207ee93be945e778e32d01a
FeepingCreature <feepingcreature@gmail.com>
2024-03-13 18:27:48 +0100

    Allow specifying the module to run unittests for. By default, only run unittests for the root module.

--- unittest.sh
+++ unittest.sh
@@ -5,7 +5,7 @@ FLAGS="-lm -lpthread -j8"
 RUN="build/unittest"
 
 if [ "${1:-}" = "win64" ]; then
-    FLAGS="${FLAGS} -target x86_64-w64-mingw32"
+    FLAGS="${FLAGS} --target x86_64-w64-mingw32"
     RUN="wine build/unittest.exe"
     function filter() {
         grep -v ' neat' |grep -v ' backend' |grep -v 'main' | grep -v 'std.macro' |\
@@ -29,5 +29,5 @@ mkdir -p build
 ) > build/unittest.nt
 
 
-neat -unittest -no-main $PACKAGES $FLAGS build/unittest.nt -o build/unittest
+neat --unittest=src --no-main $PACKAGES $FLAGS build/unittest.nt -o build/unittest
 eval "${RUN}"
